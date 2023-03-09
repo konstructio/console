bec3c0a0332d5d030f23079a4ce4d505ebd1381a
CristhianF7 <CristhianF7@gmail.com>
2023-03-09 11:48:55 -0500

    fix: typo and eol

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Document, { DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-export default class MetaphorDocument extends Document {
+export default class ConsoleDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
