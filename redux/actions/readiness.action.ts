bf6a672a65603519d14736824f55b7165e8efa17
Cristhian Fernández <CristhianF7@gmail.com>
2023-03-09 14:13:27 -0500

    fix: cors check (#137)

--- redux/actions/readiness.action.ts
+++ redux/actions/readiness.action.ts
@@ -1,7 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import https from 'https';
-
-import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { endpoints } from '../api/';
@@ -9,16 +6,12 @@ import { setMetaphorValidSite } from '../../redux/slices/readiness.slice';
 
 const { readiness } = endpoints;
 
-const httpsAgent = new https.Agent({
-  rejectUnauthorized: false,
-});
-
 export const checkReadiness = createAsyncThunk<any, any>(
   'readiness/check',
   async (body, { dispatch }) => {
     if (body.url.includes('localdev.me')) {
       try {
-        await axios.get(body.url, { httpsAgent: httpsAgent });
+        await fetch(body.url, { mode: 'no-cors' });
         return dispatch(setMetaphorValidSite({ url: body.url }));
       } catch (error) {
         // supressing error for local
