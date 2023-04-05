a3281ebb785d6c21940dd8e8004629131d8e841b
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-04-05 16:06:58 -0700

    chore: remove selectors. state selection exposed by useAppSelector.

--- redux/selectors/readiness.selector.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-import { ReadinessState } from '../slices/readiness.slice';
-import { RootState } from '../store';
-
-const readinessSelector = (state: RootState): ReadinessState => state.readiness;
-
-export const selectMetaphorValidUrls = () =>
-  createSelector(readinessSelector, ({ metaphorValidSites }) => metaphorValidSites || []);
