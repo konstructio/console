4cc49f52d6ff5569258639b139a3612b6e6ca15e
Cristhian Fernández <CristhianF7@gmail.com>
2023-06-06 14:53:44 -0500

    feat telemetry api (#199)
    * feat: global clusterId
    
    * feat: telemetry api

--- server.js
+++ server.js
@@ -5,13 +5,10 @@ const http = require('http');
 
 const NextServer = require('next/dist/server/next-server').default;
 const defaultNextConfig = require('next/dist/server/config-shared').defaultConfig;
-const Analytics = require('analytics-node');
 process.env.NODE_ENV = 'production';
 process.chdir(__dirname);
 
 const nextConfig = require('./next.config');
-
-const { HEARTBEAT_PERIOD_MINUTES } = process.env;
 
 process.on('SIGTERM', () => process.exit(0));
 process.on('SIGINT', () => process.exit(0));
@@ -29,41 +26,6 @@ const server = http.createServer(async (req, res) => {
   }
 });
 const currentPort = parseInt(process.env.PORT, 10) || 3000;
-
-const ANALYTICS_ID = '0gAYkX5RV3vt7s4pqCOOsDb6WHPLT30M';
-
-function sendHeartbeat() {
-  const { CLUSTER_ID, CLUSTER_TYPE, DISABLE_TELEMETRY, KUBEFIRST_VERSION } = process.env;
-
-  try {
-    const isTelemetryDisabled = DISABLE_TELEMETRY === 'true';
-    const analytics = new Analytics(ANALYTICS_ID, {
-      enable: !isTelemetryDisabled,
-    });
-
-    if (!isTelemetryDisabled) {
-      analytics.identify({
-        userId: CLUSTER_ID,
-      });
-
-      analytics.track({
-        userId: CLUSTER_ID,
-        event: 'kubefirst.console.healthz',
-        properties: {
-          cli_version: KUBEFIRST_VERSION,
-          cluster_id: CLUSTER_ID,
-          cluster_type: CLUSTER_TYPE,
-        },
-      });
-    }
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log('error sending hearbeat event', error);
-  }
-}
-
-sendHeartbeat();
-setInterval(sendHeartbeat, (HEARTBEAT_PERIOD_MINUTES || 20) * 60 * 1000);
 
 server.listen(currentPort, (err) => {
   if (err) {
