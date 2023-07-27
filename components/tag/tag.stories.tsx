421d7ff2cb94e839b8db6e18c261af4e77939a77
Derrick <76881293+D-B-Hawk@users.noreply.github.com>
2023-07-27 11:23:06 -0700

    refactor: update tag component to have optional icon if needed. (#218)

--- components/tag/tag.stories.tsx
+++ components/tag/tag.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Story } from '@storybook/react';
 
-import Tag, { TAG_COLOR_OPTIONS, TagProps } from '.';
+import Tag, { TAG_COLOR_OPTIONS, TagProps, TAG_ICON_OPTONS } from '.';
 
 export default {
   title: 'Components/Tag',
@@ -10,6 +10,10 @@ export default {
     bgColor: {
       control: 'select',
       options: TAG_COLOR_OPTIONS,
+    },
+    icon: {
+      control: 'radio',
+      options: Object.keys(TAG_ICON_OPTONS),
     },
   },
 };
