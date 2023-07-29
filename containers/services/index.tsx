ef1d82913e38854c18737a998db9cd0b6a2ba87b
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-07-28 18:28:26 -0700

    chore: stub out workload cluster pop out menu

--- containers/services/index.tsx
+++ containers/services/index.tsx
@@ -1,9 +1,10 @@
-import React, { FunctionComponent, useEffect, useCallback, useState, useMemo } from 'react';
+import React, { FunctionComponent, useEffect, useCallback, useState, useMemo, useRef } from 'react';
 import { Box, Tabs } from '@mui/material';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { CSSTransition } from 'react-transition-group';
 
 import Service from '../service';
-import GitOpsCatalog from '../gitOpsCatalog';
 import TabPanel, { Tab, a11yProps } from '../../components/tab';
 import Typography from '../../components/typography';
 import useFeatureFlag from '../../hooks/useFeatureFlag';
@@ -15,16 +16,31 @@ import {
 import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { DOCS_LINK } from '../../constants';
 import { BISCAY, SALTBOX_BLUE, VOLCANIC_SAND } from '../../constants/colors';
+import Button from '../../components/button';
+import { Flow } from '../../components/flow';
+import Column from '../../components/column';
+import closeImageSrc from '../../assets/close.svg';
 
-import { Container, Content, Header, LearnMoreLink, ServicesContainer } from './services.styled';
+import {
+  CloseButton,
+  ClusterMenu,
+  ClusterMenuFooter,
+  Container,
+  Content,
+  Header,
+  LearnMoreLink,
+  MenuHeader,
+  ServicesContainer,
+} from './services.styled';
 
 enum SERVICES_TABS {
-  PROVISIONED = 0,
-  GITOPS_CATALOG = 1,
+  LIST_VIEW = 0,
+  GRAPH_VIEW = 1,
 }
 
 const Services: FunctionComponent = () => {
   const [activeTab, setActiveTab] = useState<number>(0);
+  const [showPanel, setShowPanel] = useState(false);
   const router = useRouter();
 
   const { isEnabled: isGitOpsCatalogEnabled } = useFeatureFlag('gitops-catalog');
@@ -87,46 +103,70 @@ const Services: FunctionComponent = () => {
     [clusterServices, onClickLink],
   );
 
+  const nodeRef = useRef<HTMLDivElement>(null);
+
   return (
     <Container>
-      <Header>
-        <Typography variant="h6">Services Overview</Typography>
-      </Header>
       {isGitOpsCatalogEnabled ? (
         <>
-          <Box sx={{ width: 'fit-content', mb: 4 }}>
-            <Tabs value={activeTab} onChange={handleChange} indicatorColor="primary">
-              <Tab
-                color={activeTab === SERVICES_TABS.PROVISIONED ? BISCAY : SALTBOX_BLUE}
-                label={<Typography variant="buttonSmall">Provisioned services</Typography>}
-                {...a11yProps(SERVICES_TABS.PROVISIONED)}
-                sx={{ textTransform: 'initial', mr: 3 }}
-              />
+          <Header>
+            <Box sx={{ width: 'fit-content', marginLeft: '39px' }}>
+              <Tabs value={activeTab} onChange={handleChange} indicatorColor="primary">
+                <Tab
+                  color={activeTab === SERVICES_TABS.LIST_VIEW ? BISCAY : SALTBOX_BLUE}
+                  label={<Typography variant="buttonSmall">List view</Typography>}
+                  {...a11yProps(SERVICES_TABS.LIST_VIEW)}
+                  sx={{ textTransform: 'initial', mr: 3 }}
+                />
 
-              <Tab
-                color={activeTab === SERVICES_TABS.GITOPS_CATALOG ? BISCAY : SALTBOX_BLUE}
-                label={<Typography variant="buttonSmall">GitOps catalog</Typography>}
-                {...a11yProps(SERVICES_TABS.GITOPS_CATALOG)}
-                sx={{ textTransform: 'initial' }}
-              />
-            </Tabs>
-          </Box>
+                <Tab
+                  color={activeTab === SERVICES_TABS.GRAPH_VIEW ? BISCAY : SALTBOX_BLUE}
+                  label={<Typography variant="buttonSmall">Graph view</Typography>}
+                  {...a11yProps(SERVICES_TABS.GRAPH_VIEW)}
+                  sx={{ textTransform: 'initial' }}
+                />
+              </Tabs>
+            </Box>
+            <Button
+              color="primary"
+              variant="contained"
+              style={{ marginRight: '24px' }}
+              onClick={() => setShowPanel(!showPanel)}
+            >
+              Add workload cluster
+            </Button>
+          </Header>
+          <CSSTransition
+            nodeRef={nodeRef}
+            in={showPanel}
+            timeout={500}
+            classNames="cluster-menu"
+            unmountOnExit
+          >
+            <ClusterMenu ref={nodeRef}>
+              <MenuHeader>
+                <Typography variant="subtitle2">Create workload cluster</Typography>
+                <CloseButton onClick={() => setShowPanel(false)}>
+                  <Image src={closeImageSrc} height={24} width={24} alt="close" />
+                </CloseButton>
+              </MenuHeader>
+              <Column style={{ flex: 1 }}>{/* TODO: ADD CLUSTER FORM */}</Column>
+              <ClusterMenuFooter>
+                <Button variant="outlined" color="primary" onClick={() => setShowPanel(false)}>
+                  Close
+                </Button>
+                <Button variant="contained" color="primary">
+                  Create cluster
+                </Button>
+              </ClusterMenuFooter>
+            </ClusterMenu>
+          </CSSTransition>
           <Content>
-            <TabPanel value={activeTab} index={SERVICES_TABS.PROVISIONED}>
+            <TabPanel value={activeTab} index={SERVICES_TABS.LIST_VIEW}>
               {services}
             </TabPanel>
-            <TabPanel value={activeTab} index={SERVICES_TABS.GITOPS_CATALOG}>
-              <Typography variant="body2" sx={{ mb: 3 }} color={VOLCANIC_SAND}>
-                Add the latest version of your favourite application to your cluster.{' '}
-                <LearnMoreLink
-                  href={DOCS_LINK}
-                  target="_blank"
-                  onClick={() => onClickLink(DOCS_LINK, 'docs')}
-                >
-                  Learn more
-                </LearnMoreLink>
-              </Typography>
-              <GitOpsCatalog />
+            <TabPanel value={activeTab} index={SERVICES_TABS.GRAPH_VIEW}>
+              <Flow />
             </TabPanel>
           </Content>
         </>
--- containers/services/services.styled.ts
+++ containers/services/services.styled.ts
@@ -1,27 +1,77 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  height: calc(100vh - 64px);
-  margin: 0 auto;
-  overflow: auto;
-  padding: 40px 0 0 40px;
-  width: calc(100% - 40px);
+import Row from '../../components/row';
+import Column from '../../components/column';
+
+export const ClusterMenu = styled(Column)`
+  border-left: 1px solid #e2e8f0;
+  height: 100%;
+  width: 684px;
+  transition: all 0.4s ease;
+  background-color: white;
+  position: absolute;
+  display: none;
+  top: 0;
+  right: 0;
+  z-index: 1;
+
+  &.cluster-menu-enter-done,
+  &.cluster-menu-exit {
+    display: flex;
+    transform: translateX(0px);
+  }
+  &.cluster-menu-enter-active,
+  &.cluster-menu-exit-active {
+    display: flex;
+    transform: translateX(684px);
+  }
 `;
 
-export const Content = styled.div``;
+export const ClusterMenuFooter = styled(Row)`
+  justify-content: flex-end;
+  gap: 16px;
+  padding-right: 24px;
+  align-items: center;
+  height: 88px;
+  border-top: 1px solid #e2e8f0;
+`;
 
-export const Header = styled.div`
+export const CloseButton = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+export const Container = styled(Column)`
+  flex: 1;
+  margin: 0 auto;
+  overflow: hidden;
+  width: 100%;
+`;
+
+export const Content = styled(Column)`
+  flex: 1;
+  width: 100%;
+`;
+
+export const Header = styled(Row)`
   color: ${({ theme }) => theme.colors.volcanicSand};
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  margin-bottom: 24px;
+  height: 70px;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  border-top: 1px solid #e2e8f0;
 `;
 
 export const LearnMoreLink = styled(Link)`
   color: ${({ theme }) => theme.colors.primary};
   text-decoration: none;
+`;
+
+export const MenuHeader = styled(Header)`
+  padding: 0 24px;
+  border-bottom: 1px solid #e2e8f0;
 `;
 
 export const ServicesContainer = styled.div`
