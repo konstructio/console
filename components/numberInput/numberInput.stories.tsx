60877ea58631faf03d6f2475ff892c4edd7b6bd6
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-07-31 13:53:23 -0700

    chore: add number input needed for cluster management form

---
+++ components/numberInput/numberInput.stories.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Story } from '@storybook/react';
+
+import NumberInput from '.';
+
+export default {
+  title: 'Components/NumberInput',
+  component: NumberInput,
+};
+
+const inputMinimum = 0;
+
+const DefaultTemplate: Story = (args) => {
+  const [value, setValue] = useState(inputMinimum);
+  return (
+    <NumberInput
+      value={value}
+      min={inputMinimum}
+      onIncrease={() => setValue((curVal) => curVal + 1)}
+      onDecrease={() => setValue((curVal) => (curVal === inputMinimum ? curVal : curVal - 1))}
+      style={{ margin: '50px auto' }}
+    />
+  );
+};
+
+export const Default = DefaultTemplate.bind({});
