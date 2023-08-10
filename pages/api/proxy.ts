1fc97f8f9f5c7aa936230cc74d1a15b6396c00f1
Cristhian Fernández <CristhianF7@gmail.com>
2023-08-10 14:28:28 -0500

    feat: api authentication (#228)

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { API_URL = '' } = process.env;
+  const { API_URL = '', K1_ACCESS_TOKEN = '' } = process.env;
   const { body, url } = req.body;
   const { url: queryUrl } = req.query;
 
@@ -15,7 +15,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   // eslint-disable-next-line no-console
   console.log(`METHOD: ${req.method} URL: ${kubefirstEndpointUrl}`);
   try {
-    const response = await axios({ url: kubefirstEndpointUrl, data: body, method: req.method });
+    const response = await axios({
+      url: kubefirstEndpointUrl,
+      data: body,
+      method: req.method,
+      headers: {
+        ...req.headers,
+        Authorization: `Bearer ${K1_ACCESS_TOKEN}`,
+      },
+    });
     res.status(200).json(response.data);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
