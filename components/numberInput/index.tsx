02b702c558f144e895f0e266bf2befd0c512259c
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-08-14 21:26:52 -0700

    refactor: forwardRef for number input and create controlled number input to work with react-hook-form controller

--- components/numberInput/index.tsx
+++ components/numberInput/index.tsx
@@ -1,43 +1,63 @@
-import React, { FunctionComponent, ComponentPropsWithoutRef, InputHTMLAttributes } from 'react';
+import React, { ComponentPropsWithoutRef, InputHTMLAttributes, useState, forwardRef } from 'react';
 import Image from 'next/image';
 import styled from 'styled-components';
 
 import removeIconSrc from '../../assets/remove.svg';
 import addIconSrc from '../../assets/add.svg';
+import { noop } from '../../utils/noop';
+import Typography from '../../components/typography';
+import { EXCLUSIVE_PLUM } from '../../constants/colors';
 
-import { Root, NumInput, InputContainer, LabelContainer } from './numberInput.styled';
+import { Root, NumInput, InputContainer, LabelContainer, Asterisk } from './numberInput.styled';
 
-interface NumberInputProps extends ComponentPropsWithoutRef<'label'> {
+export interface NumberInputProps extends Omit<ComponentPropsWithoutRef<'label'>, 'onChange'> {
   label?: string;
-  onIncrease: () => void;
-  onDecrease: () => void;
-  inputProps: InputHTMLAttributes<HTMLInputElement>;
+  inputProps?: InputHTMLAttributes<HTMLInputElement>;
+  onChange?: (value: number) => void;
 }
 
-const NumberInput: FunctionComponent<NumberInputProps> = ({
-  onIncrease,
-  onDecrease,
-  label,
-  inputProps,
-  ...rest
-}) => {
+const NumberInput = forwardRef<HTMLLabelElement | null, NumberInputProps>(function NumberInput(
+  { label, onChange = noop, inputProps = {}, ...rest },
+  ref,
+) {
+  const [value, setValue] = useState(Number(inputProps.min ?? 0));
   return (
-    <Root {...rest}>
-      <LabelContainer>
-        {label}
-        {inputProps.required && <span>*</span>}
-      </LabelContainer>
+    <Root {...rest} ref={ref}>
+      {label && (
+        <LabelContainer>
+          <Typography variant="labelLarge" color={EXCLUSIVE_PLUM}>
+            {label}
+          </Typography>
+          {inputProps.required && <Asterisk>*</Asterisk>}
+        </LabelContainer>
+      )}
       <InputContainer>
-        <button type="button" onClick={onDecrease}>
+        <button
+          type="button"
+          onClick={() => {
+            const curValue = value - 1;
+            setValue(curValue);
+            onChange(curValue);
+          }}
+          disabled={value === inputProps.min || inputProps.disabled}
+        >
           <Image src={removeIconSrc} alt="remove" width={20} height={20} />
         </button>
-        <NumInput {...inputProps} />
-        <button type="button" onClick={onIncrease}>
+        <NumInput {...inputProps} value={value} readOnly />
+        <button
+          type="button"
+          onClick={() => {
+            const curValue = value + 1;
+            setValue(curValue);
+            onChange(curValue);
+          }}
+          disabled={inputProps.disabled}
+        >
           <Image src={addIconSrc} alt="add" width={20} height={20} />
         </button>
       </InputContainer>
     </Root>
   );
-};
+});
 
 export default styled(NumberInput)``;
