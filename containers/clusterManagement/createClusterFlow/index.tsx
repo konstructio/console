35e05a650353963fbf58ca3d3c2cddb77004650d
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-08-15 11:11:36 -0700

    miscellaneous things. mvp request for cluster creation.

--- containers/clusterManagement/createClusterFlow/index.tsx
+++ containers/clusterManagement/createClusterFlow/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useCallback, useRef, useState } from 'react';
+import React, { FunctionComponent, useCallback } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import { FormProvider, useForm } from 'react-hook-form';
 
@@ -7,25 +7,21 @@ import Button from '../../../components/button';
 import { SALTBOX_BLUE } from '../../../constants/colors';
 import Column from '../../../components/column';
 import LinearProgress from '../../../components/linearProgress';
-import ClusterCreationForm, {
-  ClusterConfig,
-} from '../../../containers/clusterForms/clusterCreation';
+import ClusterCreationForm from '../../../containers/clusterForms/clusterCreation';
 import TerminalLogs from '../../../containers/terminalLogs';
 import ClusterDetails from '../../../components/clusterDetails';
 import { ClusterInfo } from '../../../components/clusterTable/clusterTable';
+import { useAppDispatch, useAppSelector } from '../../../redux/store';
+import { ClusterCreationStep, NewClusterConfig } from '../../../types/provision';
+import { createWorkloadCluster } from '../../../redux/thunks/api.thunk';
+import { setClusterCreationStep } from '../../../redux/slices/api.slice';
 
-import { CloseButton, ClusterMenuFooter, MenuHeader } from './createClusterFlow.styled';
+import { CloseButton, ClusterMenuFooter, Form, MenuHeader } from './createClusterFlow.styled';
 
-enum CLUSTER_MANAGEMENT_STEP {
-  CREATE,
-  PROVISION,
-  DETAILS,
-}
-
-const actionButtonText: Record<CLUSTER_MANAGEMENT_STEP, string> = {
-  [CLUSTER_MANAGEMENT_STEP.CREATE]: 'Create cluster',
-  [CLUSTER_MANAGEMENT_STEP.PROVISION]: 'View cluster details',
-  [CLUSTER_MANAGEMENT_STEP.DETAILS]: 'Delete Cluster',
+const actionButtonText: Record<ClusterCreationStep, string> = {
+  [ClusterCreationStep.CONFIG]: 'Create cluster',
+  [ClusterCreationStep.PROVISION]: 'View cluster details',
+  [ClusterCreationStep.DETAILS]: 'Delete Cluster',
 };
 
 interface CreateClusterFlowProps {
@@ -37,71 +33,74 @@ export const CreateClusterFlow: FunctionComponent<CreateClusterFlowProps> = ({
   cluster,
   onMenuClose,
 }) => {
-  const [workloadClusterStep, setWorkloadClusterStep] = useState<CLUSTER_MANAGEMENT_STEP>(
-    CLUSTER_MANAGEMENT_STEP.CREATE,
-  );
+  const { clusterCreationStep } = useAppSelector(({ api }) => api);
 
-  const methods = useForm<ClusterConfig>();
+  const dispatch = useAppDispatch();
 
-  const formRef = useRef<HTMLFormElement>(null);
+  const methods = useForm<NewClusterConfig>();
 
   const handleMenuClose = useCallback(() => {
+    // if provisioning, keep menu on provisioning step
+    if (clusterCreationStep !== ClusterCreationStep.PROVISION) {
+      dispatch(setClusterCreationStep(ClusterCreationStep.CONFIG));
+    }
     onMenuClose();
-    setWorkloadClusterStep((curStep) =>
-      curStep === CLUSTER_MANAGEMENT_STEP.PROVISION ? curStep : CLUSTER_MANAGEMENT_STEP.CREATE,
-    );
-  }, [onMenuClose, setWorkloadClusterStep]);
+  }, [dispatch, clusterCreationStep, onMenuClose]);
 
   const handleClick = useCallback(() => {
-    setWorkloadClusterStep((curStep) =>
-      curStep === CLUSTER_MANAGEMENT_STEP.DETAILS ? curStep : curStep + 1,
-    );
-  }, [setWorkloadClusterStep]);
+    if (clusterCreationStep !== ClusterCreationStep.DETAILS && methods.formState.isValid) {
+      dispatch(setClusterCreationStep(clusterCreationStep + 1));
+    }
+  }, [dispatch, clusterCreationStep, methods]);
 
-  const showingClusterDetails = workloadClusterStep === CLUSTER_MANAGEMENT_STEP.DETAILS;
+  const handleSubmit = useCallback(
+    (config: NewClusterConfig) => {
+      dispatch(createWorkloadCluster(config));
+    },
+    [dispatch],
+  );
+
+  const showingClusterDetails = clusterCreationStep === ClusterCreationStep.DETAILS;
 
   return (
-    <>
-      <MenuHeader>
-        <Typography variant="subtitle2">
-          {showingClusterDetails ? cluster?.clusterName : 'Create workload cluster'}
-        </Typography>
-        <CloseButton onClick={handleMenuClose}>
-          <CloseIcon htmlColor={SALTBOX_BLUE} />
-        </CloseButton>
-      </MenuHeader>
-      <Column style={{ flex: 1, padding: '0 24px' }}>
-        {workloadClusterStep === CLUSTER_MANAGEMENT_STEP.CREATE && (
-          <FormProvider {...methods}>
-            <ClusterCreationForm
-              onFormSubmit={(config) => console.log('the values =>', config)}
-              ref={formRef}
-              style={{ height: '100%', marginTop: '32px' }}
-            />
-          </FormProvider>
-        )}
-        {workloadClusterStep === CLUSTER_MANAGEMENT_STEP.PROVISION && (
-          <Column style={{ gap: '32px' }}>
-            <LinearProgress progress={80} />
-            <TerminalLogs />
-          </Column>
-        )}
-        {workloadClusterStep === CLUSTER_MANAGEMENT_STEP.DETAILS && cluster && (
-          <ClusterDetails cluster={cluster} style={{ marginTop: '24px' }} />
-        )}
-      </Column>
-      <ClusterMenuFooter reverseButtonOrder={showingClusterDetails}>
-        <Button variant="outlined" color="primary" onClick={handleMenuClose}>
-          Close
-        </Button>
-        <Button
-          variant="contained"
-          color={showingClusterDetails ? 'error' : 'primary'}
-          onClick={handleClick}
-        >
-          {actionButtonText[workloadClusterStep]}
-        </Button>
-      </ClusterMenuFooter>
-    </>
+    <FormProvider {...methods}>
+      <Form onSubmit={methods.handleSubmit(handleSubmit)}>
+        <MenuHeader>
+          <Typography variant="subtitle2">
+            {showingClusterDetails ? cluster?.clusterName : 'Create workload cluster'}
+          </Typography>
+          <CloseButton onClick={handleMenuClose} type="button">
+            <CloseIcon htmlColor={SALTBOX_BLUE} />
+          </CloseButton>
+        </MenuHeader>
+        <Column style={{ flex: 1, padding: '0 24px', overflow: 'auto' }}>
+          {clusterCreationStep === ClusterCreationStep.CONFIG && (
+            <ClusterCreationForm style={{ flex: 1, margin: '32px 0' }} />
+          )}
+          {clusterCreationStep === ClusterCreationStep.PROVISION && (
+            <Column style={{ gap: '32px' }}>
+              <LinearProgress progress={80} />
+              <TerminalLogs />
+            </Column>
+          )}
+          {clusterCreationStep === ClusterCreationStep.DETAILS && cluster && (
+            <ClusterDetails cluster={cluster} style={{ marginTop: '24px' }} />
+          )}
+        </Column>
+        <ClusterMenuFooter reverseButtonOrder={showingClusterDetails}>
+          <Button variant="outlined" color="primary" onClick={handleMenuClose} type="button">
+            Close
+          </Button>
+          <Button
+            variant="contained"
+            color={showingClusterDetails ? 'error' : 'primary'}
+            onClick={handleClick}
+            type="submit"
+          >
+            {actionButtonText[clusterCreationStep]}
+          </Button>
+        </ClusterMenuFooter>
+      </Form>
+    </FormProvider>
   );
 };
