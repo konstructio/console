a1ddd57085ece409810dbb6e0331dc59a3733915
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-08-17 23:00:54 -0700

    chore: hydrate nodes and edges for react flow redux state. use implicit type attached to return of getClusters instead of explicit.

--- redux/thunks/api.thunk.ts
+++ redux/thunks/api.thunk.ts
@@ -15,45 +15,10 @@ import {
 import { GitOpsCatalogApp, GitOpsCatalogProps } from '../../types/gitOpsCatalog';
 import { InstallValues, InstallationType } from '../../types/redux';
 import { TelemetryClickEvent } from '../../types/telemetry';
-
-const mapClusterFromRaw = (cluster: ClusterResponse): Cluster => ({
-  id: cluster._id,
-  clusterName: cluster.cluster_name,
-  adminEmail: cluster.alerts_email,
-  cloudProvider: cluster.cloud_provider,
-  cloudRegion: cluster.cloud_region,
-  domainName: cluster.domain_name,
-  gitAuth: cluster.gitAuth,
-  gitProvider: cluster.git_provider,
-  gitUser: cluster.git_user,
-  type: cluster.cluster_type,
-  creationDate: cluster.creation_timestamp,
-  lastErrorCondition: cluster.last_condition,
-  status: cluster.status,
-  vaultAuth: {
-    kbotPassword: cluster.vault_auth?.kbot_password,
-  },
-  checks: {
-    install_tools_check: cluster.install_tools_check,
-    domain_liveness_check: cluster.domain_liveness_check,
-    state_store_creds_check: cluster.state_store_creds_check,
-    state_store_create_check: cluster.state_store_create_check,
-    git_init_check: cluster.git_init_check,
-    kbot_setup_check: cluster.kbot_setup_check,
-    gitops_ready_check: cluster.gitops_ready_check,
-    git_terraform_apply_check: cluster.git_terraform_apply_check,
-    gitops_pushed_check: cluster.gitops_pushed_check,
-    cloud_terraform_apply_check: cluster.cloud_terraform_apply_check,
-    cluster_secrets_created_check: cluster.cluster_secrets_created_check,
-    argocd_install_check: cluster.argocd_install_check,
-    argocd_initialize_check: cluster.argocd_initialize_check,
-    argocd_create_registry_check: cluster.argocd_create_registry_check,
-    argocd_delete_registry_check: cluster.argocd_delete_registry_check,
-    vault_initialized_check: cluster.vault_initialized_check,
-    vault_terraform_apply_check: cluster.vault_terraform_apply_check,
-    users_terraform_apply_check: cluster.users_terraform_apply_check,
-  },
-});
+import { sortClustersByType } from '../../utils/sortClusterByType';
+import { generateNodesConfig } from '../../utils/reactFlow';
+import { mapClusterFromRaw } from '../../utils/mapClustersFromRaw';
+import { setEdges, setNodes } from '../../redux/slices/reactFlow.slice';
 
 export const createCluster = createAsyncThunk<
   Cluster,
@@ -152,20 +117,33 @@ export const getCluster = createAsyncThunk<
 });
 
 export const getClusters = createAsyncThunk<
-  Array<Cluster>,
+  ReturnType<typeof sortClustersByType>,
   void,
   {
     dispatch: AppDispatch;
     state: RootState;
   }
->('api/cluster/getClusters', async () => {
-  const res = await axios.get(`/api/proxy?${createQueryString('url', `/cluster`)}`);
+>('api/cluster/getClusters', async (_, { dispatch }) => {
+  const res = await axios.get<ClusterResponse[]>(
+    `/api/proxy?${createQueryString('url', `/cluster`)}`,
+  );
 
   if ('error' in res) {
     throw res.error;
   }
 
-  return (res.data && res.data.map(mapClusterFromRaw)) || [];
+  // make response ingestable
+  const mappedClusters = res.data.map(mapClusterFromRaw);
+  // sort management from workload clusters
+  const { managementCluster, workloadClusters } = sortClustersByType(mappedClusters);
+  // create node/edge config
+  if (managementCluster) {
+    const [nodes, edges] = generateNodesConfig(managementCluster, workloadClusters);
+    dispatch(setNodes(nodes));
+    dispatch(setEdges(edges));
+  }
+
+  return { managementCluster, workloadClusters };
 });
 
 export const deleteCluster = createAsyncThunk<
