82203d6ad6c81a1eddf1ac3b87946b3074fd15ff
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-08-22 18:54:44 -0700

    refactor: add optional selected prop to generatedNode func. add optional animated prop to generateEdge func.

--- utils/reactFlow/index.ts
+++ utils/reactFlow/index.ts
@@ -4,9 +4,10 @@ import { Cluster } from '../../types/provision';
 import { CustomGraphNode } from '../../components/graphNode';
 
 export function generateNode(
-  cluster: Cluster,
   id: string,
   position: { x: number; y: number },
+  cluster: Partial<Cluster>,
+  selected = false,
 ): CustomGraphNode {
   return {
     id,
@@ -14,15 +15,16 @@ export function generateNode(
     data: cluster,
     position,
     draggable: false,
+    selected,
   };
 }
 
-export function generateEdge(id: string, source: string, target: string): Edge {
+export function generateEdge(id: string, source: string, target: string, animated = false): Edge {
   return {
     id,
     source,
     target,
-    animated: false,
+    animated,
     type: 'straight',
     style: { strokeWidth: 2, stroke: '#CBD5E1' },
   };
@@ -34,10 +36,14 @@ export function generateNodesConfig(
 ): [CustomGraphNode[], Edge[]] {
   const managementNodeId = '1';
   const nodes: CustomGraphNode[] = [
-    generateNode(managementCluster, managementNodeId, {
-      x: 0,
-      y: (workloadClusters.length * 200) / 2,
-    }),
+    generateNode(
+      managementNodeId,
+      {
+        x: 0,
+        y: (workloadClusters.length * 200) / 2,
+      },
+      managementCluster,
+    ),
   ];
 
   const edges: Edge[] = [];
@@ -45,7 +51,7 @@ export function generateNodesConfig(
   for (let i = 0; i < workloadClusters.length; i += 1) {
     const generatedId = `${i + 2}`;
 
-    nodes.push(generateNode(workloadClusters[i], generatedId, { x: 600, y: i * 200 }));
+    nodes.push(generateNode(generatedId, { x: 600, y: i * 200 }, workloadClusters[i]));
     edges.push(generateEdge(`edge-${i + 1}`, managementNodeId, generatedId));
   }
 
