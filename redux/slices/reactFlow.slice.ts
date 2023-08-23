a8de2aa903baed7adadaf0ec7c7a055b406f3e76
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-08-22 18:58:39 -0700

    chore: pull draft node into view when creating workload cluster/or when node is in the provisioning stage. remove any selected nodes when create workload cluster form flow is closed.

--- redux/slices/reactFlow.slice.ts
+++ redux/slices/reactFlow.slice.ts
@@ -6,12 +6,15 @@ import {
   Connection,
   Edge,
   EdgeChange,
-  Node,
   NodeChange,
 } from 'reactflow';
 
+import { CustomGraphNode } from '../../components/graphNode';
+import { ClusterType } from '../../types/provision';
+import { generateEdge, generateNode } from '../../utils/reactFlow';
+
 export interface ReactFlowState {
-  nodes: Node[];
+  nodes: CustomGraphNode[];
   edges: Edge[];
 }
 
@@ -24,11 +27,17 @@ const reactFlowSlice = createSlice({
   name: 'react-flow',
   initialState,
   reducers: {
-    setNodes: (state, { payload }: PayloadAction<Node[]>) => {
+    setNodes: (state, { payload }: PayloadAction<CustomGraphNode[]>) => {
       state.nodes = payload;
+    },
+    addNode: (state, { payload }: PayloadAction<CustomGraphNode>) => {
+      state.nodes = [...state.nodes, payload];
     },
     setEdges: (state, { payload }: PayloadAction<Edge[]>) => {
       state.edges = payload;
+    },
+    addNewEdge: (state, { payload }: PayloadAction<Edge>) => {
+      state.edges = addEdge(payload, state.edges);
     },
     onNodesChange: (state, { payload }: PayloadAction<NodeChange[]>) => {
       state.nodes = applyNodeChanges(payload, state.nodes);
@@ -39,10 +48,57 @@ const reactFlowSlice = createSlice({
     onConnect: (state, { payload }: PayloadAction<Connection>) => {
       state.edges = addEdge(payload, state.edges);
     },
+    generateDraftNode: (state) => {
+      const managementNode = state.nodes.find((node) => node.data.type === ClusterType.MANAGEMENT);
+      // last node position
+      const { position } = state.nodes[state.nodes.length - 1];
+
+      if (managementNode) {
+        const {
+          id,
+          data: { cloudProvider },
+        } = managementNode;
+
+        const draftNode = generateNode(
+          'draft',
+          { ...position, y: position.y + 200 },
+          { cloudProvider },
+          true,
+        );
+
+        const draftEdge = generateEdge('edge-draft', id, draftNode.id, true);
+
+        state.nodes = [...state.nodes, draftNode];
+        state.edges = addEdge(draftEdge, state.edges);
+      }
+    },
+    removeDraftNode: (state) => {
+      state.edges = state.edges.filter((edge) => !edge.id.includes('draft'));
+      state.nodes = state.nodes.filter((node) => node.id !== 'draft');
+    },
+    unSelectNodes: (state) => {
+      state.nodes = state.nodes.map((node) => ({ ...node, selected: false }));
+    },
+    setDraftNodeActive: (state) => {
+      state.nodes = state.nodes.map((node) =>
+        node.id === 'draft' ? { ...node, selected: true } : node,
+      );
+    },
   },
 });
 
-export const { setNodes, setEdges, onNodesChange, onEdgesChange, onConnect } =
-  reactFlowSlice.actions;
+export const {
+  setNodes,
+  addNode,
+  setEdges,
+  addNewEdge,
+  onNodesChange,
+  onEdgesChange,
+  onConnect,
+  generateDraftNode,
+  removeDraftNode,
+  unSelectNodes,
+  setDraftNodeActive,
+} = reactFlowSlice.actions;
 
 export const reactFlowReducer = reactFlowSlice.reducer;
