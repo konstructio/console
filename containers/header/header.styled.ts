7de1c4b19dadc5931de616dfe68ab690c9097e83
Cristhian Fernández <CristhianF7@gmail.com>
2023-09-04 12:16:50 -0500

    fix: mgmt tweaks (#259)

--- containers/header/header.styled.ts
+++ containers/header/header.styled.ts
@@ -11,22 +11,3 @@ export const Container = styled(row)`
   justify-content: center;
   width: 100%;
 `;
-
-export const ClusterIndicator = styled.div`
-  width: 8px;
-  height: 8px;
-  border-radius: 4px;
-  background: #22c55e;
-`;
-
-export const ClusterMenu = styled.div`
-  align-items: center;
-  background: #fafafa;
-  border: 1px solid #f4f4f5;
-  display: flex;
-  height: 28px;
-  gap: 8px;
-  justify-content: center;
-  padding: 0 8px;
-  text-transform: uppercase;
-`;
