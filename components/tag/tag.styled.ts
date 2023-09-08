98ff8cecb00c55d1e1d602aa39edef203aa41ddd
Derrick <76881293+D-B-Hawk@users.noreply.github.com>
2023-09-08 12:58:51 -0700

    refactor: add option for removable tag for selectable lists (#263)
    * refactor: add option for removable tag for selectable lists
    
    * fix: add fallback for ignored or unexpanded type

--- components/tag/tag.styled.ts
+++ components/tag/tag.styled.ts
@@ -1,88 +1,36 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import Image from 'next/image';
 
 import Row from '../row/';
 
-import { TagColor } from '.';
+export const IconImage = styled(Image)`
+  margin-right: 4px;
+`;
 
-export const TagContainer = styled(Row)<{ bgColor?: TagColor }>`
+export const RemovalButton = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  padding: 0;
+  margin-left: 8px;
+  height: 14px;
+  width: 14px;
+`;
+
+export const TagContainer = styled(Row)<{ bg?: string; textColor?: string }>`
+  align-items: flex-end;
   border-radius: 4px;
   padding: 4px;
   text-transform: capitalize;
-  width: fit-content;
+  background-color: ${({ bg }) => bg};
+  color: ${({ textColor }) => textColor};
 
-  ${({ bgColor }) =>
-    bgColor === 'neon-green'
-      ? css`
-          background-color: #ecfccb;
-          color: #4d7c0f;
-        `
-      : bgColor === 'light-orange'
-      ? css`
-          background-color: #fef3c7;
-          color: #d97706;
-        `
-      : bgColor === 'pink'
-      ? css`
-          background-color: #fce7f3;
-          color: #be185d;
-        `
-      : bgColor === 'fuscia'
-      ? css`
-          background-color: #fdf4ff;
-          color: #a21caf;
-        `
-      : bgColor === 'light-blue'
-      ? css`
-          background-color: #ecfeff;
-          color: #0e7490;
-        `
-      : bgColor === 'sky-blue'
-      ? css`
-          background-color: #e0f2fe;
-          color: #0369a1;
-        `
-      : bgColor === 'dark-sky-blue'
-      ? css`
-          background-color: #dbeafe;
-          color: #1d4ed8;
-        `
-      : bgColor === 'purple'
-      ? css`
-          background-color: #ede9fe;
-          color: #6d28d9;
-        `
-      : bgColor === 'yellow'
-      ? css`
-          background-color: #fef9c3;
-          color: #a16207;
-        `
-      : bgColor === 'green'
-      ? css`
-          background-color: #dcfce7;
-          color: #15803d;
-        `
-      : bgColor === 'emerald'
-      ? css`
-          background-color: #ecfdf5;
-          color: #047857;
-        `
-      : bgColor === 'grey'
-      ? css`
-          background-color: #f8fafc;
-          color: #71717a;
-        `
-      : bgColor === 'indigo'
-      ? css`
-          background-color: #e0e7ff;
-          color: #4338ca;
-        `
-      : css`
-          background-color: none;
-          color: #71717a;
-        `}
-`;
-
-export const IconImage = styled(Image)`
-  margin-right: 4px;
+  ${RemovalButton} {
+    & svg {
+      padding: 0;
+      height: 14px;
+      width: 14px;
+      color: ${({ textColor }) => textColor};
+    }
+  }
 `;
