cff3a27482f7a3ab03de213d209c1272d0755823
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-10-18 10:56:01 -0700

    use material ui click away listener to clean up props and function of table

--- components/environmentsTable/environmentsTable.stories.tsx
+++ components/environmentsTable/environmentsTable.stories.tsx
@@ -1,10 +1,6 @@
-import React, { useEffect, useState } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 
 import { mockEnvironmentsResponse } from '../../tests/mocks/mockEnvironmentsResponse';
-import { useAppDispatch, useAppSelector } from '../../redux/store';
-import { setEnvironments } from '../../redux/slices/environments.slice';
-import { ClusterEnvironment } from '../../types/provision';
 import { noop } from '../../utils/noop';
 import { createEnvMap } from '../../utils/createEnvMap';
 
@@ -18,31 +14,9 @@ const meta: Meta<typeof EnvironmentsTable> = {
 
 export default meta;
 
-const EnvironmentsTableWithHooks = () => {
-  const [selectedEnv, setSelectedEnv] = useState<ClusterEnvironment>();
-  const { environments } = useAppSelector(({ environments }) => environments);
-
-  const dispatch = useAppDispatch();
-
-  const handleMenuButtonClick = (env: ClusterEnvironment) => {
-    setSelectedEnv((curEnv) => (curEnv?.name === env.name ? undefined : env));
-  };
-
-  useEffect(() => {
-    dispatch(setEnvironments(mockEnvironments));
-  }, [dispatch]);
-
-  return (
-    <EnvironmentsTable
-      environments={environments}
-      onDeleteEnvironment={noop}
-      onMenuButtonClick={handleMenuButtonClick}
-      selectedEnvironment={selectedEnv}
-      // onEditEnvironment={noop}
-    />
-  );
-};
-
 export const Default: StoryObj<typeof EnvironmentsTable> = {
-  render: () => <EnvironmentsTableWithHooks />,
+  args: {
+    onDeleteEnvironment: noop,
+    environments: mockEnvironments,
+  },
 };
