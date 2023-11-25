695a8358d7a161041e1ef2f3e8d5dca1e65bcc55
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-11-24 18:42:08 -0800

    use default values passed along to keep things as dry as possible.

--- containers/clusterManagement/createClusterFlow/index.tsx
+++ containers/clusterManagement/createClusterFlow/index.tsx
@@ -26,9 +26,6 @@ import {
   MenuHeader,
 } from './createClusterFlow.styled';
 
-import { InstallationType } from '@/types/redux';
-import { DEFAULT_CLOUD_INSTANCE_SIZES } from '@/constants';
-
 const actionButtonText: Record<ClusterCreationStep, string> = {
   [ClusterCreationStep.CONFIG]: 'Create cluster',
   [ClusterCreationStep.DETAILS]: 'Delete cluster',
@@ -59,14 +56,8 @@ export const CreateClusterFlow: FunctionComponent<CreateClusterFlowProps> = ({
   notifiedOfBetaPhysicalClusters,
   onNotificationClose,
 }) => {
-  const { instanceSize } =
-    DEFAULT_CLOUD_INSTANCE_SIZES[managementCluster?.cloudProvider ?? InstallationType.LOCAL];
-
   const methods = useForm<NewWorkloadClusterConfig>({
-    defaultValues: {
-      ...defaultValues,
-      instanceSize,
-    },
+    defaultValues,
     mode: 'onChange',
   });
 
