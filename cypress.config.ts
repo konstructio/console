e3c13e5dc8517d33f19979c9e3fdb20df6025ae9
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-12-11 15:38:45 -0800

    disable unused vars for injected setUpNodeEvents func

--- cypress.config.ts
+++ cypress.config.ts
@@ -64,6 +64,7 @@ export default defineConfig({
   viewportHeight: 900,
 
   e2e: {
+    //eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
