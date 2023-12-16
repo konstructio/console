1f10b0780643e3971b367b391e353417d0427efd
Derrick <76881293+D-B-Hawk@users.noreply.github.com>
2023-12-15 16:00:16 -0800

    update next@13.5.6. add defaults to context (#423)

--- hooks/useQueue/queue.context.ts
+++ hooks/useQueue/queue.context.ts
@@ -2,11 +2,16 @@
 import { createContext } from 'react';
 
 import { ClusterQueue } from '@/types/provision';
+import { noop } from '@/utils/noop';
 
 export interface QueueContextProps {
   addClusterToQueue: (clusterQueue: ClusterQueue) => void;
   deleteClusterFromQueue: (clusterId: string) => void;
 }
 
-const QueueContext = createContext<QueueContextProps>({} as QueueContextProps);
+const QueueContext = createContext<QueueContextProps>({
+  addClusterToQueue: noop,
+  deleteClusterFromQueue: noop,
+});
+
 export default QueueContext;
