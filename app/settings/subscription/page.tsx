b381db5aa70d67b96ec3a085c445c92474f43fba
CristhianF7 <CristhianF7@gmail.com>
2023-12-19 13:17:01 -0500

    fix: types

--- app/settings/subscription/page.tsx
+++ app/settings/subscription/page.tsx
@@ -3,16 +3,14 @@ import axios from 'axios';
 import sortBy from 'lodash/sortBy';
 
 import Subscription from '@/containers/subscription';
+import { Plan } from '@/types/plan';
 
 async function getProductPlans() {
   const { SAAS_API_URL = '' } = process.env;
 
-  const response = await axios.get(`${SAAS_API_URL}/api/v1/payment/plans`);
+  const { data } = await axios.get<Array<Plan>>(`${SAAS_API_URL}/api/v1/payment/plans`);
 
-  return sortBy(
-    response.data,
-    (product) => product && product.metadata && product.metadata['order'],
-  );
+  return sortBy(data, (product) => product && product.metadata && product.metadata['order']);
 }
 
 export default async function Page() {
