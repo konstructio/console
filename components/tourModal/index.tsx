36ed952ac0f91bf6b11ee233e3d0a2acf3bf9b6a
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2024-01-12 17:21:09 -0800

    add tourModal comp/story

---
+++ components/tourModal/index.tsx
@@ -0,0 +1,43 @@
+import React, { FunctionComponent } from 'react';
+
+import Modal, { IModalProps } from '../modal';
+import Typography from '../typography';
+import Button from '../button';
+
+import { CardContent, CardFooter, Content, K1Ray } from './tourModal.styled';
+
+import kubefirstRay from '@/assets/ray.svg';
+import { BISCAY, DOLPHIN } from '@/constants/colors';
+
+interface TourModalProps extends Omit<IModalProps, 'children'> {
+  onSkip: () => void;
+  onTakeTour: () => void;
+}
+
+const TourCard: FunctionComponent<TourModalProps> = ({ onSkip, onTakeTour, ...modalProps }) => {
+  return (
+    <Modal {...modalProps}>
+      <Content>
+        <CardContent>
+          <Typography variant="h6" color={BISCAY}>
+            Welcome to kubefirst!
+          </Typography>
+          <K1Ray src={kubefirstRay} alt="Kubefirst k ray" />
+          <Typography variant="body2" color={DOLPHIN} textAlign="center">
+            We’d like to show you a couple of the features we’ve included to get you started.
+          </Typography>
+        </CardContent>
+        <CardFooter>
+          <Button color="text" onClick={onSkip}>
+            Skip
+          </Button>
+          <Button color="primary" variant="contained" onClick={onTakeTour}>
+            Take a quick tour
+          </Button>
+        </CardFooter>
+      </Content>
+    </Modal>
+  );
+};
+
+export default TourCard;
