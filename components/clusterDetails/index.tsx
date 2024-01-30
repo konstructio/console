f17cf947db211b9b9c7a97bda30365edf7677451
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2024-01-30 14:29:08 -0700

    update cluster details to match updated designs

--- components/clusterDetails/index.tsx
+++ components/clusterDetails/index.tsx
@@ -10,15 +10,15 @@ import moment from 'moment';
 import {
   Container,
   Content,
-  ColumnInfo,
   RowInfo,
   StyledLabel,
   StyledValue,
   ExternalLink,
+  StyledDivider,
+  StatusContainer,
 } from './clusterDetails.styled';
 
 import Typography from '@/components/typography';
-import Column from '@/components/column';
 import {
   Cluster,
   ClusterStatus,
@@ -27,7 +27,6 @@ import {
   ManagementCluster,
 } from '@/types/provision';
 import StatusIndicator from '@/components/statusIndicator';
-import { BISCAY, VOLCANIC_SAND } from '@/constants/colors';
 import { CLOUD_PROVIDER_DISPLAY_NAME, GIT_PROVIDER_DISPLAY_NAME } from '@/constants';
 import Tag from '@/components/tag';
 
@@ -73,95 +72,82 @@ const ClusterDetails: FunctionComponent<ClusterDetailsProps> = ({
 
   return (
     <Container {...rest}>
-      <Column style={{ gap: '8px' }}>
-        <Typography variant="subtitle1" color={BISCAY}>
-          {clusterName}
-        </Typography>
-        {environment?.name && <Tag text={environment.name} bgColor={environment.color} />}
-        {environment?.description && (
-          <Typography variant="body2" color={VOLCANIC_SAND}>
-            {environment.description}
-          </Typography>
-        )}
-        <Column style={{ gap: '4px' }}>
-          <StatusIndicator available>
-            <ExternalLink href={`https://argocd.${fullDomainName}/applications/clusters`} available>
-              View your Argo CD clusters
-            </ExternalLink>
-          </StatusIndicator>
-          <StatusIndicator available={available}>
-            <ExternalLink
-              href={`https://${host}/${gitOwner}/gitops/tree/main/registry/clusters/${clusterName}`}
-              available={available}
-            >
-              View your {gitProvider} cluster configuration
-            </ExternalLink>
-          </StatusIndicator>
-        </Column>
-      </Column>
+      <StatusContainer>
+        <StatusIndicator available>
+          <ExternalLink href={`https://argocd.${fullDomainName}/applications/clusters`} available>
+            View your Argo CD clusters
+          </ExternalLink>
+        </StatusIndicator>
+        <StatusIndicator available={available}>
+          <ExternalLink
+            href={`https://${host}/${gitOwner}/gitops/tree/main/registry/clusters/${clusterName}`}
+            available={available}
+          >
+            View your {gitProvider} cluster configuration
+          </ExternalLink>
+        </StatusIndicator>
+      </StatusContainer>
+
+      <StyledDivider />
+
+      <Typography style={{ margin: '16px 0' }} variant="subtitle3">
+        Details
+      </Typography>
 
       <Content>
-        {/* Top Row */}
+        {/* Domain name */}
         <RowInfo>
-          <ColumnInfo>
-            <StyledLabel variant="labelLarge">Cluster domain name</StyledLabel>
-            <StyledValue variant="body2">{fullDomainName}</StyledValue>
-          </ColumnInfo>
-          <ColumnInfo>
-            <StyledLabel variant="labelLarge">Alerts email</StyledLabel>
-            <StyledValue variant="body2">{adminEmail}</StyledValue>
-          </ColumnInfo>
+          <StyledLabel variant="labelLarge">Cluster domain name</StyledLabel>
+          <StyledValue variant="body2">{fullDomainName}</StyledValue>
         </RowInfo>
-
-        {/* Second Row */}
+        {/* Git provider */}
         <RowInfo>
-          <ColumnInfo>
-            <StyledLabel variant="labelLarge">Created</StyledLabel>
-            <StyledValue variant="body2" data-test-id="creation-date">
-              {creationDate && moment(+creationDate).format('DD MMM YYYY, HH:MM:SS')}
-            </StyledValue>
-          </ColumnInfo>
-          <ColumnInfo>
-            <StyledLabel variant="labelLarge">Created by</StyledLabel>
-            <StyledValue variant="body2">{gitUser}</StyledValue>
-          </ColumnInfo>
+          <StyledLabel variant="labelLarge">Git provider</StyledLabel>
+          <StyledValue variant="body2">{GIT_PROVIDER_DISPLAY_NAME[gitProvider]}</StyledValue>
         </RowInfo>
-
-        {/* Third Row */}
+        {/* Environments */}
         <RowInfo>
-          <ColumnInfo>
-            <StyledLabel variant="labelLarge">Git provider</StyledLabel>
-            <StyledValue variant="body2">{GIT_PROVIDER_DISPLAY_NAME[gitProvider]}</StyledValue>
-          </ColumnInfo>
-          <ColumnInfo>
-            <StyledLabel variant="labelLarge">Cloud provider</StyledLabel>
-            <StyledValue variant="body2">{CLOUD_PROVIDER_DISPLAY_NAME[cloudProvider]}</StyledValue>
-          </ColumnInfo>
+          <StyledLabel variant="labelLarge">Environments</StyledLabel>
+          {environment?.name && <Tag text={environment.name} bgColor={environment.color} />}
         </RowInfo>
-
-        {/* Fourth Row */}
+        {/* Cloud account */}
         <RowInfo>
-          <ColumnInfo>
-            <StyledLabel variant="labelLarge">Cloud region</StyledLabel>
-            <StyledValue variant="body2">{cloudRegion}</StyledValue>
-          </ColumnInfo>
-          {type !== ClusterType.WORKLOAD_V_CLUSTER && (
-            <ColumnInfo>
-              <StyledLabel variant="labelLarge">Number of nodes</StyledLabel>
-              <StyledValue variant="body2">{nodeCount}</StyledValue>
-            </ColumnInfo>
-          )}
+          <StyledLabel variant="labelLarge">Cloud account</StyledLabel>
+          <StyledValue variant="body2">{CLOUD_PROVIDER_DISPLAY_NAME[cloudProvider]}</StyledValue>
         </RowInfo>
-
-        {/* Fifth Row */}
-
+        {/* Cloud region */}
         <RowInfo>
-          <ColumnInfo>
-            <StyledLabel variant="labelLarge">Instance size</StyledLabel>
-            <StyledValue variant="body2" style={{ width: '100%' }}>
-              {instanceSize?.toUpperCase()}
-            </StyledValue>
-          </ColumnInfo>
+          <StyledLabel variant="labelLarge">Cloud region</StyledLabel>
+          <StyledValue variant="body2">{cloudRegion}</StyledValue>
+        </RowInfo>
+        {/* Instance size */}
+        <RowInfo>
+          <StyledLabel variant="labelLarge">Instance size</StyledLabel>
+          {instanceSize && <StyledValue variant="body2">{instanceSize.toUpperCase()}</StyledValue>}
+        </RowInfo>
+        {/* Number of nodes */}
+        {type !== ClusterType.WORKLOAD_V_CLUSTER && (
+          <RowInfo>
+            <StyledLabel variant="labelLarge">Number of nodes</StyledLabel>
+            <StyledValue variant="body2">{nodeCount}</StyledValue>
+          </RowInfo>
+        )}
+        {/* Alerts email */}
+        <RowInfo>
+          <StyledLabel variant="labelLarge">Alerts email</StyledLabel>
+          <StyledValue variant="body2">{adminEmail}</StyledValue>
+        </RowInfo>
+        {/* Created */}
+        <RowInfo>
+          <StyledLabel variant="labelLarge">Created</StyledLabel>
+          <StyledValue variant="body2">
+            {creationDate && moment(+creationDate).format('DD MMM YYYY, HH:MM:SS')}
+          </StyledValue>
+        </RowInfo>
+        {/* Created by */}
+        <RowInfo>
+          <StyledLabel variant="labelLarge">Created by</StyledLabel>
+          <StyledValue variant="body2">{gitUser}</StyledValue>
         </RowInfo>
       </Content>
     </Container>
