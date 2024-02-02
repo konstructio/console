4ef8b40a74e9f1faa3c0886817bcb46db645b3cb
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2024-02-02 09:21:30 -0700

    create KubeConfigModal to display cli commands for download of kubeconfig for aws, gcp

---
+++ components/kubeConfigModal/index.tsx
@@ -0,0 +1,58 @@
+import React, { FunctionComponent } from 'react';
+
+import Modal, { IModalProps } from '../modal';
+import Column from '../column';
+import CopyCommand, { CopyCommandProps } from '../copyCommand';
+import Button from '../button';
+
+import {
+  ButtonContainer,
+  Content,
+  ExclamationIcon,
+  ExternalLink,
+  Main,
+  StyledTypography,
+} from './kubeConfigModal.styled';
+
+export interface KubeConfigModalProps extends Omit<IModalProps, 'children'>, CopyCommandProps {
+  onAcceptance: () => void;
+  commandDocLink: string;
+}
+
+const KubeConfigModal: FunctionComponent<KubeConfigModalProps> = ({
+  command,
+  onAcceptance,
+  commandDocLink,
+  ...rest
+}) => {
+  return (
+    <Modal padding={0} styleOverrides={{ padding: '32px', width: '630px' }} {...rest}>
+      <Main>
+        {/* Exclamation Icon */}
+        <Column>
+          <ExclamationIcon />
+        </Column>
+        {/* Main Content */}
+        <Content>
+          <StyledTypography variant="subtitle2">
+            Access your kubeconfig file by using the following command:
+          </StyledTypography>
+          <StyledTypography variant="body2">
+            You can find more information about accessing your clusters kubeconfig file{' '}
+            <ExternalLink href={commandDocLink} target="_blank" rel="noreferrer">
+              here.
+            </ExternalLink>
+          </StyledTypography>
+          <CopyCommand command={command} />
+          <ButtonContainer>
+            <Button variant="contained" color="primary" onClick={onAcceptance}>
+              Got it
+            </Button>
+          </ButtonContainer>
+        </Content>
+      </Main>
+    </Modal>
+  );
+};
+
+export default KubeConfigModal;
