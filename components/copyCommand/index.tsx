bbcfc420d170c08ea806befa482b3b2d594c1b62
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2024-02-02 09:12:32 -0700

    create CopyCommand comp

---
+++ components/copyCommand/index.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react';
+
+import CopyButton from '../copyButton';
+
+import { Command, Container } from './copyCommand.styled';
+
+export interface CopyCommandProps {
+  command: string;
+}
+
+const CopyCommand: FunctionComponent<CopyCommandProps> = ({ command, ...rest }) => {
+  return (
+    <Container {...rest}>
+      <Command>{command}</Command>
+      <CopyButton buttonText="Copy" textToCopy={command} />
+    </Container>
+  );
+};
+
+export default CopyCommand;
